9421e71b30bc1c814809001adf10ea565b406346
John Northrup <john@8bitwizard.net>
2016-04-01 19:22:49 -0500

    removing old homebrew version

--- homebrew/Library/Homebrew/cmd/--prefix.rb
+++
@@ -1,9 +0,0 @@
-module Homebrew extend self
-  def __prefix
-    if ARGV.named.empty?
-      puts HOMEBREW_PREFIX
-    else
-      puts ARGV.formulae.map{ |f| f.installed_prefix }
-    end
-  end
-end
--- homebrew/Library/Homebrew/cmd/link.rb
+++
@@ -1,8 +0,0 @@
-module Homebrew extend self
-  def link
-    ARGV.kegs.each do |keg|
-      print "Linking #{keg}... "
-      puts "#{keg.link} links created"
-    end
-  end
-end
